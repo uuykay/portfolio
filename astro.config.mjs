a6d6a5b1c99fcae9c22a4e9ea08a08e824991bb7
Will <william.y.kuang@gmail.com>
2023-08-26 20:44:42 +1000

    Set up astro images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from "astro/config";
 import { BASE_ROUTE } from "./src/constants/routes";
 import tailwind from "@astrojs/tailwind";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), image()],
   site: "https://uuykay.github.io",
   base: BASE_ROUTE,
   experimental: {
-    viewTransitions: true,
-  },
+    viewTransitions: true
+  }
 });
